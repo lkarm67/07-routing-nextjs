d825b79638e0c0678a5536e76247dc070a84756a
Luidmyla Karmeliuk <lkarm67@gmail.com>
2025-10-08 18:51:20 +0300

    change routs

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,4 +1,4 @@
-import NotePreview from "@/components/NotePreview/NotePreview";
+import NotePreview from "@/app/@modal/(.)notes/[id]/NotePreview";
 
 export default function NoteModalPage() {
   return <NotePreview />;
